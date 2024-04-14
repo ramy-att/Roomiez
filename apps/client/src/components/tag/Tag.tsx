a3503ce3f8dd564073db4b7a678d539ca386b23d
Ramy Attalla <83047251+ramy-att@users.noreply.github.com>
2024-04-13 22:10:25 -0400

    onclick

--- apps/client/src/components/tag/Tag.tsx
+++ apps/client/src/components/tag/Tag.tsx
@@ -2,7 +2,15 @@ import React, { useState } from "react";
 import checkmark from "../../assets/checkmark.svg";
 import close from "../../assets/close.svg";
 
-export const Tag = ({ text }: { text: string }) => {
+export const Tag = ({
+  text,
+  onClick,
+  value,
+}: {
+  text: string;
+  value: string;
+  onClick: (value?: string) => void;
+}) => {
   const [clicked, setClicked] = useState(false);
 
   return (
@@ -12,7 +20,10 @@ export const Tag = ({ text }: { text: string }) => {
           ? "bg-primary text-white cursor-pointer transition duration-300 ease-in-out"
           : "bg-gray-200 text-gray-700 cursor-pointer transition duration-300 ease-in-out"
       } hover:bg-opacity-75`}
-      onClick={() => setClicked((prev) => !prev)}
+      onClick={() => {
+        setClicked((prev) => !prev);
+        onClick?.(value);
+      }}
     >
       {text}
       <img width="10" src={!clicked ? checkmark : close} />
